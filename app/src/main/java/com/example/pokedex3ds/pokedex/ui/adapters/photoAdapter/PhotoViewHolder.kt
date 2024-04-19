3c8ad9581e9907856620ea14cefbcb1805899608
Damián Pérez <122502579+M4n-Co@users.noreply.github.com>
2024-04-18 22:11:10 -0600

    pequeña correccion

--- app/src/main/java/com/example/pokedex3ds/pokedex/ui/adapters/photoAdapter/PhotoViewHolder.kt
+++ app/src/main/java/com/example/pokedex3ds/pokedex/ui/adapters/photoAdapter/PhotoViewHolder.kt
@@ -3,7 +3,6 @@ package com.example.pokedex3ds.pokedex.ui.adapters.photoAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.pokedex3ds.databinding.ItemPokemonPhotoBinding
-import com.squareup.picasso.Picasso
 
 class PhotoViewHolder ( private  val mBinding : ItemPokemonPhotoBinding) : RecyclerView.ViewHolder(mBinding.root) {
 
@@ -11,7 +10,6 @@ class PhotoViewHolder ( private  val mBinding : ItemPokemonPhotoBinding) : Recyc
         Glide.with(mBinding.ivPhoto.context)
             .load(url)
             .into(mBinding.ivPhoto)
-//        Picasso.get().load(url).into(mBinding.ivPhoto)
     }
 
 }
